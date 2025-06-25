6e785e2aeec7a28232a4d7950e3a237050bf5f0a
jc033 <jc033@BOOK-T43CJUDFP3>
2025-06-25 22:07:35 +0530

    Pushed By Balaram

---
+++ Balaram_Framework_Git/src/test/java/createContactTest/ToCreateContactTest.java
@@ -0,0 +1,8 @@
+package createContactTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("Contact Created By Balaram");
+	}
+}
---
+++ Balaram_Framework_Git/src/test/java/createOgrTest/ToCreateOrgTest.java
@@ -0,0 +1,8 @@
+package createOgrTest;
+
+public class ToCreateOrgTest {
+
+	public static void main(String[] args) {
+		System.out.println("Organization Created By Balaram");
+	}
+}

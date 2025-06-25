dc5869617fa194da549688f9492c7c73b5f02905
lenovo <lenovo@LAPTOP-1HCIDQMG>
2025-06-25 22:19:05 +0530

    New Push calender organisation contact

--- Balaram_Framework_Git/src/test/java/createOgrTest/ToCreateOrgTest.java
+++ Balaram_Framework_Git/src/test/java/createOgrTest/ToCreateOrgTest.java
@@ -4,5 +4,6 @@ public class ToCreateOrgTest {
 
 	public static void main(String[] args) {
 		System.out.println("Organization Created By Balaram");
+		System.out.println("Organisation created By Sourav");
 	}
 }
